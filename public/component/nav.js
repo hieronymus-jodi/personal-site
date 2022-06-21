7bc38044c729bf58d17d5a713ec3535f556d6968
hieronymus-jodi <jhieronymus@nevada.unr.edu>
2022-06-21 15:37:53 -0700

    Setup pages, add navbar

---
+++ public/component/nav.js
@@ -0,0 +1,12 @@
+// Adds navbar to site
+
+document.write('<ul class="navbar">\
+<li class="navbar-item">\<a href="/">\Home</a>\</li>\
+<li class="navbar-item">\<a href="about">\About</a>\</li>\
+<li class="navbar-item">\<a href="resume">\Resume</a>\</li>\
+<li class="navbar-item">\<a href="github">\GitHub</a>\</li>\
+<li class="navbar-item">\<a href="contact">\Contact</a>\</li>\
+<li class="navbar-item">\<a href="weekly-learning-log">\Weekly Learning</a>\</li>\
+<li class="navbar-item">\<a href="japanese">\自己紹介</a>\</li>\
+</ul>\
+');
---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+
+// Imports
+const http = require('http')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const server = http.createServer(app)
+const port = 3939
+
+// Static files
+app.use(express.json())
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/component', express.static(__dirname + 'public/component'))
+
+app.set('pages', './pages')
+
+// Website Urls
+app.use('', function(req,res){
+    res.sendFile(path.join(__dirname + '/pages/homepage.html'))
+})
+
+app.use('/about', function(req,res){
+    res.sendFile(path.join(__dirname + '/pages/about.html'))
+})
+
+app.use(express.static("source"))
+
+server.listen(port, () => console.info(`Listening on port ${port}`))
